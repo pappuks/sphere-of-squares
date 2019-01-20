6a2c972db69524b1e30dedfddac88dce1aba64d9
Kuldeep <pappuks@gmail.com>
2019-01-20 09:44:52 -0800

    First files

---
+++ SphereOfSquares/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SphereOfSquares {
+    public partial class Form1 : Form {
+        private const int NumberOfSquaresInTotal = 400;
+        private const int NumberOfSquaresInACircle = 20;
+        private Square[] squares = new Square[NumberOfSquaresInTotal];
+        private Queue<int> queue = new Queue<int>(NumberOfSquaresInTotal);
+        private ManualResetEvent queueEvent = new ManualResetEvent(false);
+        private ManualResetEvent doneDrawing = new ManualResetEvent(false);
+        private double radius;
+        Pen myPen = new Pen(Color.Blue, 2);
+
+        public Form1() {
+            InitializeComponent();
+            this.Size = new Size(600, 700);
+            InitializeSphere();
+        }
+
+        private void InitializeSphere() {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(QueueEventHandler));
+            ThreadPool.QueueUserWorkItem(new WaitCallback(CreateAllSquares));
+        }
+
+        private void CreateAllSquares(object state) {
+            // First calculate the radius of the sphere
+            radius = (Math.Min(Size.Height, Size.Width) - 20) / 2;
+            double centerAngle = 360 / NumberOfSquaresInACircle;
+            int indexor = 0;
+            for (int i = 0; i < NumberOfSquaresInACircle; i++) {
+                for (int j = 0; j < NumberOfSquaresInACircle; j++) {
+                    squares[indexor] =
+                    new Square(
+                        radius,
+                        0,
+                        centerAngle,
+                        new Square.XYAngle(i * centerAngle, j * centerAngle),
+                        queue,
+                        queueEvent,
+                        indexor);
+                    indexor++;
+                }
+            }
+            //while (indexor < NumberOfSquaresInACircle) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(indexor * centerAngle, 0),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (2 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(0, (indexor - NumberOfSquaresInACircle) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (3 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(90, (indexor - (2 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (4 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(centerAngle, (indexor - (3 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (5 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(-centerAngle, (indexor - (4 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (6 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(2 * centerAngle, (indexor - (5 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (7 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(-2 * centerAngle, (indexor - (6 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (8 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(3 * centerAngle, (indexor - (7 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (9 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(-3 * centerAngle, (indexor - (8 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (10 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(4 * centerAngle, (indexor - (9 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+            //while (indexor < (11 * NumberOfSquaresInACircle)) {
+            //    squares[indexor] =
+            //        new Square(
+            //            radius,
+            //            0,
+            //            centerAngle,
+            //            new Square.XYAngle(-4 * centerAngle, (indexor - (10 * NumberOfSquaresInACircle)) * centerAngle),
+            //            queue,
+            //            queueEvent,
+            //            indexor);
+            //    indexor++;
+            //}
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            //if (queue.Count > 0) {
+            //    Pen myPen = new Pen(Color.Blue, 1);
+            //    Graphics gfx = e.Graphics;
+            //    do {
+            //        int value = queue.Dequeue();
+            //        PointF[] coords = squares[value].Coordinates;
+            //        // Draw the coordinates
+            //        gfx.DrawLine(myPen, coords[0], coords[1]);
+            //        gfx.DrawLine(myPen, coords[1], coords[2]);
+            //        gfx.DrawLine(myPen, coords[2], coords[3]);
+            //        gfx.DrawLine(myPen, coords[3], coords[0]);
+            //    } while (queue.Count > 0);
+            //}
+            
+            Graphics gfx = e.Graphics;
+            for (int i = 0; i < NumberOfSquaresInTotal; i++) {
+                if (squares[i] != null && squares[i].IsVisible && squares[i].IsDirty) {
+                    PointF[] coords = squares[i].Coordinates;
+                    // Draw the coordinates
+                    gfx.DrawPolygon(myPen, coords);
+                    gfx.FillPolygon(squares[i].MyBrush, coords);
+                    //gfx.DrawLine(myPen, coords[0], coords[1]);
+                    //gfx.DrawLine(myPen, coords[1], coords[2]);
+                    //gfx.DrawLine(myPen, coords[2], coords[3]);
+                    //gfx.DrawLine(myPen, coords[3], coords[0]);
+                    squares[i].IsDirty = false;
+                }
+            }
+            doneDrawing.Set();
+            base.OnPaint(e);
+        }
+
+        private void QueueEventHandler(object state) {
+            while (true) {
+                queueEvent.WaitOne();
+                doneDrawing.Reset();
+                this.Invalidate();
+                doneDrawing.WaitOne();
+                queueEvent.Reset();
+            }
+        }
+
+        private Rectangle GetRectangle(PointF[] coords) {
+            float xMin = Math.Min(coords[0].X, coords[3].X);
+            float xMax = Math.Max(coords[1].X, coords[2].X);
+            float yMin = Math.Min(coords[0].Y, coords[1].Y);
+            float yMax = Math.Max(coords[2].Y, coords[3].Y);
+            return new Rectangle((int)xMin, (int)yMin, (int)(xMax - xMin), (int)(yMax - yMin));
+
+        }
+        private bool mouseDown = false;
+        private Point mouseDownPoint;
+        
+        protected override void OnMouseDown(MouseEventArgs e) {
+            mouseDown = true;
+            mouseDownPoint = e.Location;
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e) {
+            if (mouseDown) {
+                Point currentLocation = e.Location;
+                Point mouseMovement = new Point(currentLocation.X - mouseDownPoint.X, currentLocation.Y - mouseDownPoint.Y);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(UpdateAllSquares), mouseMovement);
+            }
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e) {
+            mouseDown = false;
+            base.OnMouseUp(e);
+        }
+
+        private void UpdateAllSquares(object state) {
+            Point mouseMovement = (Point)state;
+            for (int i = 0; i < NumberOfSquaresInTotal; i++) {
+                squares[i].XYAngleValue =
+                    new Square.XYAngle(
+                        squares[i].XYAngleValue.XAxis + (mouseMovement.X / 10),
+                        squares[i].XYAngleValue.YAxis + (mouseMovement.Y / 10));
+            }
+            //this.Invalidate();
+        }
+
+    }
+}
---
+++ SphereOfSquares/Square.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading;
+
+namespace SphereOfSquares {
+    class Square {
+
+        public class XYAngle {
+            private double xAxisAngle;
+            private double yAxisAngle;
+
+            public XYAngle(double xAxis, double yAxis) {
+                xAxisAngle = xAxis;
+                yAxisAngle = yAxis;
+            }
+
+            public double XAxis {
+                get {
+                    return xAxisAngle;
+                }
+                set {
+                    xAxisAngle = value;
+                }
+            }
+
+            public double YAxis {
+                get {
+                    return yAxisAngle;
+                }
+                set {
+                    yAxisAngle = value;
+                }
+            }
+
+            public override bool Equals(object obj) {
+                XYAngle equalTo = obj as XYAngle;
+                bool retVal = false;
+                if (equalTo != null) {
+                    retVal = (equalTo.XAxis == xAxisAngle) && (equalTo.YAxis == yAxisAngle);
+                }
+ 	            return retVal;
+            }
+
+            public override int GetHashCode() {
+ 	             return base.GetHashCode();
+            }
+        }
+        
+        private double radius;
+        private double sideLength;
+        private double centerAngle;
+        private XYAngle xyAngle;
+        private static Brush[] myBrushes = new Brush[] {
+            Brushes.Violet,
+            Brushes.Indigo,
+            Brushes.Brown,
+            Brushes.Aquamarine,
+            Brushes.Black, 
+            Brushes.Green,
+            Brushes.Yellow,
+            Brushes.Blue,
+            Brushes.Orange,
+            Brushes.Beige            
+            };
+        
+        /// <summary>
+        /// The way coordinates are stored
+        /// (1)-------------------(2)
+        ///  |                     |
+        ///  |                     |
+        ///  |                     |
+        ///  |                     |
+        /// (4)-------------------(3)
+        /// </summary>
+        private PointF[] coordinates = new PointF[4];
+
+        private ManualResetEvent queueEvent;
+        private Queue<int> queue;
+        private int indexInArray;
+        private bool isVisible;
+        private Brush myBrush;
+        private bool isDirty;
+
+        public Square(
+            double radius, 
+            double sideLength, 
+            double centerAngle, 
+            XYAngle xyAngle, 
+            Queue<int> queue, 
+            ManualResetEvent queueEvent,
+            int indexInArray
+        ) {
+            this.radius = radius;
+            this.sideLength = sideLength;
+            this.centerAngle = centerAngle;
+            this.xyAngle = xyAngle;
+            this.queue = queue;
+            this.queueEvent = queueEvent;
+            this.indexInArray = indexInArray;
+            Random rnd = new Random(indexInArray);
+            this.myBrush = myBrushes[rnd.Next(myBrushes.Length - 1)];
+            isDirty = false;
+            UpdateCoordinates();
+        }
+
+        public PointF[] Coordinates {
+            get {
+                return coordinates;
+            }
+        }
+
+        public XYAngle XYAngleValue {
+            get {
+                return xyAngle;
+            }
+            set {
+                if (!xyAngle.Equals(value)) {
+                    xyAngle = null;
+                    xyAngle = value;
+                    // Fire angle change, so that we can recalculate coordinates
+                    UpdateCoordinates();
+                }
+            }
+        }
+
+        public bool IsVisible {
+            get {
+                return isVisible;
+            }
+        }
+
+        public Brush MyBrush {
+            get {
+                return myBrush;
+            }
+        }
+
+        public bool IsDirty {
+            get {
+                return isDirty;
+            }
+            set {
+                isDirty = value;
+            }
+        }
+
+        private void UpdateCoordinates() {
+
+            //double x1 =
+            //    radius * 
+            //    Math.Sin(
+            //        ConvertToRadians(xyAngle.XAxis + (centerAngle / 2))
+            //    );
+
+            // Check if the square is visible
+            isVisible = Math.Cos(ConvertToRadians(xyAngle.XAxis)) * Math.Cos(ConvertToRadians(xyAngle.YAxis)) >= 0;
+            if (isVisible) {
+                double x1 =
+                    radius *
+                    Math.Cos(
+                        ConvertToRadians(xyAngle.YAxis + (centerAngle / 2))
+                    ) *
+                    Math.Sin(
+                        ConvertToRadians(xyAngle.XAxis - (centerAngle / 2))
+                    );
+                double x2 =
+                    radius *
+                    Math.Cos(
+                        ConvertToRadians(xyAngle.YAxis + (centerAngle / 2))
+                    ) *
+                    Math.Sin(
+                        ConvertToRadians(xyAngle.XAxis + (centerAngle / 2))
+                    );
+                double x3 =
+                    radius *
+                    Math.Cos(
+                        ConvertToRadians(xyAngle.YAxis - (centerAngle / 2))
+                    ) *
+                    Math.Sin(
+                        ConvertToRadians(xyAngle.XAxis + (centerAngle / 2))
+                    );
+                double x4 =
+                    radius *
+                    Math.Cos(
+                        ConvertToRadians(xyAngle.YAxis - (centerAngle / 2))
+                    ) *
+                    Math.Sin(
+                        ConvertToRadians(xyAngle.XAxis - (centerAngle / 2))
+                    );
+                double y1 =
+                    radius *
+                    Math.Sin(
+                        ConvertToRadians(xyAngle.YAxis + (centerAngle / 2))
+                    );
+                double y2 =
+                    radius *
+                    Math.Sin(
+                        ConvertToRadians(xyAngle.YAxis - (centerAngle / 2))
+                    );
+                //double y3;
+                //double y4;
+                coordinates[0].X = (float)x1 + (float)radius;
+                coordinates[0].Y = (float)y1 + (float)radius;
+                coordinates[1].X = (float)x2 + (float)radius;
+                coordinates[1].Y = (float)y1 + (float)radius;
+                coordinates[2].X = (float)x3 + (float)radius;
+                coordinates[2].Y = (float)y2 + (float)radius;
+                coordinates[3].X = (float)x4 + (float)radius;
+                coordinates[3].Y = (float)y2 + (float)radius;
+                // Fire coordinates updates event
+                //queue.Enqueue(indexInArray);
+                queueEvent.Set();
+                isDirty = true;
+            }
+        }
+
+        private double ConvertToRadians(double degree) {
+            return (degree * Math.PI * 2) / 360;
+        }
+    }
+}
